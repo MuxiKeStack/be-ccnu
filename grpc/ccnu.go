aa42b156a85768f48b1e38ba6d496576e081eacc
bigdust <3264085417@qq.com>
2024-04-05 01:05:21 +0800

    update: ccnu service to grpc server

---
+++ grpc/ccnu.go
@@ -0,0 +1,26 @@
+package grpc
+
+import (
+	"context"
+	"github.com/MuxiKeStack/be-api/gen/proto/ccnu"
+	"github.com/MuxiKeStack/be-ccnu/service"
+	"google.golang.org/grpc"
+)
+
+type CCNUServiceServer struct {
+	ccnuv1.UnimplementedCCNUServiceServer
+	ccnu service.CCNUService
+}
+
+func NewCCNUServiceServer(ccnu service.CCNUService) *CCNUServiceServer {
+	return &CCNUServiceServer{ccnu: ccnu}
+}
+
+func (s *CCNUServiceServer) Register(server grpc.ServiceRegistrar) {
+	ccnuv1.RegisterCCNUServiceServer(server, s)
+}
+
+func (s *CCNUServiceServer) Login(ctx context.Context, request *ccnuv1.LoginRequest) (*ccnuv1.LoginResponse, error) {
+	success, err := s.ccnu.Login(ctx, request.GetStudentId(), request.GetPassword())
+	return &ccnuv1.LoginResponse{Success: success}, err
+}
